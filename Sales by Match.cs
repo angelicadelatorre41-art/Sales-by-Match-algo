f30de1782271a43cd7d1fe3b274ff9e7a8aa9f37
angelicadelatorre41-art <angelicadelatorre41@gmail.com>
2025-10-02 10:22:50 +0800

    Sales by Match.cs

---
+++ Sales by Match.cs
@@ -0,0 +1,75 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'sockMerchant' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER n
+     *  2. INTEGER_ARRAY ar
+     */
+
+    public static int sockMerchant(int n, List<int> ar)
+    {
+         // Dictionary to hold the count of each sock color
+    Dictionary<int, int> sockCount = new Dictionary<int, int>();
+
+    // Count occurrences of each sock color
+    foreach (var sock in ar)
+    {
+        if (sockCount.ContainsKey(sock))
+        {
+            sockCount[sock]++;
+        }
+        else
+        {
+            sockCount[sock] = 1;
+        }
+    }
+
+    int totalPairs = 0;
+
+    // Calculate total pairs
+    foreach (var count in sockCount.Values)
+    {
+        totalPairs += count / 2; // Each pair consists of 2 socks
+    }
+
+    return totalPairs;
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+        List<int> ar = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arTemp => Convert.ToInt32(arTemp)).ToList();
+
+        int result = Result.sockMerchant(n, ar);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
